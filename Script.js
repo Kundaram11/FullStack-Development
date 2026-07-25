200b18da8eeb6c8faf29ccff895304093db6910e
Varsha <140479140+Kundaram11@users.noreply.github.com>
2026-07-25 22:03:04 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -1,36 +1,103 @@
-document.getElementById("blogForm").addEventListener("submit", async function(event){
+const blogForm = document.getElementById("blogForm");
 
-event.preventDefault();
+if (blogForm) {
+    blogForm.addEventListener("submit", async function (event) {
 
-const title = document.getElementById("title").value.trim();
-const author = document.getElementById("author").value.trim();
-const description = document.getElementById("description").value.trim();
+        event.preventDefault();
 
-if(title === "" || author === "" || description === ""){
-    alert("Please fill all fields!");
-    return;
+        const title = document.getElementById("title").value.trim();
+        const author = document.getElementById("author").value.trim();
+        const description = document.getElementById("description").value.trim();
+
+        if (title === "" || author === "" || description === "") {
+            alert("Please fill all fields!");
+            return;
+        }
+
+        const response = await fetch("/api/blog", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title,
+                author,
+                description
+            })
+        });
+
+        const data = await response.json();
+
+        alert(data.message);
+
+        blogForm.reset();
+
+    });
+}
+async function loadBlogs() {
+
+    const response = await fetch("/api/blogs");
+    const blogs = await response.json();
+
+    const blogList = document.getElementById("blogList");
+
+    if (!blogList) return;
+    //  Blog Counter
+    const blogCount = document.getElementById("blogCount");
+    if(blogCount){
+        blogCount.textContent = `📊 Total Blogs: ${blogs.length}`;
+    }
+    // No Blogs
+    if(blogs.length === 0){
+        blogList.innerHTML = `
+        <h3 class="no-blog">
+           No blogs available
+        </h3>`;
+        return;
+    }
+
+    blogList.innerHTML = "";
+
+    blogs.forEach((blog,index) => {
+
+        blogList.innerHTML += `
+            <div class="card">
+                ${index === blogs.length-1?`<span class="badge"></span>` : ''}
+                <h2>${blog.title}</h2>
+                <p><strong>👤 Author:</strong> ${blog.author}</p>
+                <p>${blog.description}</p>
+                <p class="blog-date">📅 ${blog.date || "Today"}</p>
+            </div>
+        `;
+
+    });
+
+}
+const search = document.getElementById("search");
+
+if (search) {
+
+    search.addEventListener("input", function () {
+
+        const value = this.value.toLowerCase();
+
+        const cards = document.querySelectorAll(".card");
+
+        cards.forEach(card => {
+
+            const title = card.querySelector("h2").textContent.toLowerCase();
+            if(title.includes(value)){
+               card.style.display = "";
+            }else{
+                card.style.display ="none";
+            }
+            
+                
+
+        });
+
+    });
+
 }
 
-const response = await fetch("/api/blog",{
-
-method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body:JSON.stringify({
-title,
-author,
-description
-})
-
-});
-
-const data = await response.json();
-
-alert(data.message);
-
-document.getElementById("blogForm").reset();
-
-});
+loadBlogs();
