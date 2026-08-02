94471f20df84bbb7eaf2f389e5c382b81342b325
Kundaram11 <varshakundaram9@gmail.com>
2026-08-02 17:01:38 +0530

    Final deployment updates

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const db = require("./db");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "Frontend")));
